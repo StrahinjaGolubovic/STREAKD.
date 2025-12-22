6c2cc2e54b4a53d558143dfc303db10ffe2db283
StrahinjaGolubovic <strahinja.golubovic@icloud.com>
2025-12-22 02:10:27 +0100

    ALTCHA Implementation - Fix 2

--- types/altcha.d.ts
+++ types/altcha.d.ts
@@ -1,14 +1,18 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'altcha-widget': {
-      challengeurl?: string;
-      strings?: {
-        label?: string;
-        error?: string;
+import { HTMLAttributes } from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'altcha-widget': HTMLAttributes<HTMLElement> & {
+        challengeurl?: string;
+        strings?: {
+          label?: string;
+          error?: string;
+        };
       };
-      className?: string;
-      children?: React.ReactNode;
-    };
+    }
   }
 }
+
+export {};
 
